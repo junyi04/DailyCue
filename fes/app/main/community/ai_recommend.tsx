7af88249dba25087a0459ca655a608b59dfc1196
exkeys <rjsdn6422@naver.com>
2025-10-20 13:07:59 +0900

    끝

--- fes/app/main/community/ai_recommend.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { AIRecommendPage } from '@/components/main_screen/community/HotIssue';
-
-export default function AIRecommendScreen() {
-  return <AIRecommendPage />;
-}
--- fes/constants/config.ts
+++
@@ -1,7 +0,0 @@
-// 개발 환경 (Render 서버 사용)
-const DEV_API_URL = 'https://emotions-e598.onrender.com';
-// 프로덕션 환경 (Render)
-const PROD_API_URL = 'https://emotions-e598.onrender.com';
-
-// 현재 환경에 따라 API URL 설정
-export const API_BASE_URL = __DEV__ ? DEV_API_URL : PROD_API_URL;
